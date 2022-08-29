589392c366519c43e6a65c4161b7213d591d1baf
Priyansh Bimbisariye <No public email provided>
2022-08-29 17:37:19 +0800

    Calculate element of pascal triangle with row and position

---
+++ pascalTriangle.js
@@ -0,0 +1,10 @@
+function pascal(row, pos){
+    return pos === 0
+        ? 1
+        : row === 0 
+        ? 0
+        : pascal(row-1, pos) + pascal(row-1, pos-1);
+}
+
+// Test Cases
+display(pascal(4,3));
