3fc19c073c629c7f462136be32551174903c084b
Source Academy User <Priyansh Bimbisariye>
2022-09-09 18:42:10 +0800

    Changes made from Source Academy

--- listOperations.js
+++ listOperations.js
@@ -1,6 +1,16 @@
+const xs = list(1,2,3,4,5);
+const n = list();
+
 function append(xs, ys){
     return is_null(xs)
         ? ys
         : pair(head(xs), append(tail(xs), ys));
+}
+
+function reverse_iter(original, reversed){
+    return is_null(original)
+        ? reversed
+        : reverse_iter(tail(original),
+            pair(head(original), reversed));
 }
 
